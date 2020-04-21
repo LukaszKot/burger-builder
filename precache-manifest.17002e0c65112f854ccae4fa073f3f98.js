afdbde1aaa60e5d02569a28bddbfc999585628d0
Lukasz Kot <mistercode11@hotmail.com>
2020-04-21 15:54:50 +0200

    Updates

---
+++ precache-manifest.17002e0c65112f854ccae4fa073f3f98.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40b7cde50c284786d86e892585107f4a",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "1152fd6f63951b4ae991",
+    "url": "/burger-builder/static/css/main.d741c32e.chunk.css"
+  },
+  {
+    "revision": "5ae4d5ac1b7f845a284a",
+    "url": "/burger-builder/static/js/2.06152986.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.06152986.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1152fd6f63951b4ae991",
+    "url": "/burger-builder/static/js/main.485fcd6c.chunk.js"
+  },
+  {
+    "revision": "aa51b73ea7754a5431dd",
+    "url": "/burger-builder/static/js/runtime-main.27f48eea.js"
+  },
+  {
+    "revision": "b8503d262bffbfb7c67fd6762963e7d1",
+    "url": "/burger-builder/static/media/burger-logo.b8503d26.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/burger-builder/precache-manifest.9981254f10030b08f4e6bbc2b2acb92d.js"
+  "/burger-builder/precache-manifest.17002e0c65112f854ccae4fa073f3f98.js"
 );
 
 self.addEventListener('message', (event) => {
