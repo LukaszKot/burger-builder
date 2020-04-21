fdd86383ecae022850c1c40f8262921b8a4c91ee
Lukasz Kot <mistercode11@hotmail.com>
2020-04-21 11:51:33 +0200

    Created burger.

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,14 @@
+import React, { Fragment } from 'react';
+import styles from './Layout.module.css'
+const Layout = (props) => (
+    <Fragment>
+        <div>
+            Toolbar, SideDrawer, Backdrop
+        </div>
+        <main className={styles.Content}>
+            {props.children}
+        </main>
+    </Fragment>
+);
+
+export default Layout;
