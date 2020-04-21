7665bc7d348f8b4199635d88a82112987b309194
Lukasz Kot <mistercode11@hotmail.com>
2020-04-21 10:35:04 +0200

    Updates

---
+++ precache-manifest.c4302a4db3df720eaa981f6a132d9323.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "471b3caef71bfbf958041cf6298f1f67",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "782000de7ec0ffdaea79",
+    "url": "/burger-builder/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3c0102c009e16e173bd4",
+    "url": "/burger-builder/static/js/2.2af88929.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.2af88929.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "782000de7ec0ffdaea79",
+    "url": "/burger-builder/static/js/main.abf37a9b.chunk.js"
+  },
+  {
+    "revision": "aa51b73ea7754a5431dd",
+    "url": "/burger-builder/static/js/runtime-main.27f48eea.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/burger-builder/static/media/logo.5d5d9eef.svg"
+  }
+]);
